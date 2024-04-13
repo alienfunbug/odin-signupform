f8bd9f527636f4ba43cbac0b11a09817090405cb
alienfunbug <alienfunbug@gmail.com>
2024-04-12 23:38:32 -0700

    removing validation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const passwordVerification = document.getElementById("user_password");
+/*const passwordVerification = document.getElementById("user_password");
 const confirmPasswordVerification = document.getElementById("confirm_user_password");
 
-/*if (passwordVerification) {
+if (passwordVerification) {
     passwordVerification.addEventListener('input',function(addEventListener){
         const inputValue = event.target.value; // Get the current value of the input field
         console.log(inputValue); // Do something with the input value
     }); 
     console.log(passwordVerification);
     console.log(confirmPasswordVerification);
-}*/
+}
 
 
 
@@ -19,4 +19,4 @@ if (passwordVerification) {
         console.log(this.value);
       
       })
-}
+}*/
