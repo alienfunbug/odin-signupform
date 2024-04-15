562164f16eaba09a71401df76a67b52ee930b0fa
alienfunbug <alienfunbug@gmail.com>
2024-04-14 18:15:31 -0700

    edit password verification css/script

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const submitButton = document.getElementById("createaccount");
 const passwordVerification = document.getElementById("user_password");
 const confirmPasswordVerification = document.getElementById("confirm_user_password");
+const passwordAlert = document.getElementById("passwordAlert");
 const signupform = document.getElementById("signupform")
-submitButton.addEventListener("click", (event) => {
 
+
+passwordVerification.addEventListener("input",(event)=>{
+	if (passwordVerification.value != confirmPasswordVerification.value){
+		passwordAlert.style.opacity=1;
+	}else{
+		passwordAlert.style.opacity=0;
+	}
+
+}); 
+
+confirmPasswordVerification.addEventListener("input",(event)=>{
+	if (passwordVerification.value != confirmPasswordVerification.value){
+		passwordAlert.style.opacity=1;
+	}else{
+		passwordAlert.style.opacity=0;
+	}
+
+});
+
+submitButton.addEventListener("click", (event) => {
     if ((passwordVerification.value === confirmPasswordVerification.value) && ((passwordVerification.value).length > 8)) {
 		alert("Successfully created account!");
         signupform.reset();
